132ab501548fdb0710412afdc77a09ccdfdd8050
David Procházka <david.prochazka@integritty.cz>
2025-05-18 16:49:27 +0200

    Product card design

--- templates/productCard.php
+++ templates/productCard.php
@@ -5,27 +5,27 @@
             <img src="public/images/product-<?php echo rand(0,1); ?>.png" width="652" height="560" alt="" class="ProductCard-image ProductCard-image--primary" loading="lazy">
         </div>
         <?php if (rand(0,1)) { ?>
-            <div class="ProductCard-primaryBadges">
-                <div class="Badge Badge--circle">
-                    -10%
-                </div>
+            <div class="ProductCard-tertiaryBadges">
+                <div class="Badge Badge--rectangle" style="--background: #132736">Novinka</div>
+                <div class="Badge Badge--rectangle" style="--background: #00519e">Doprodej</div>
+                <div class="Badge Badge--rectangle" style="--background: #d91242">Sleva -10 %</div>
             </div>
         <?php } ?>
         <?php if (rand(0,1)) { ?>
-            <div class="ProductCard-tertiaryBadges">
-                <div class="Badge Badge--rectangleSide">Pro grafiky</div>
+            <div class="ProductCard-variants">
+                <p class="ProductCard-variantsTitle">Zvolte velikost:</p>
+                <ul class="ProductCard-variantsList">
+                    <?php foreach (['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL', 'XXXXL'] as $size) { ?>
+                        <li class="ProductCard-variant">
+                            <a class="ProductCard-variantLink" href="#variant<?php echo $size; ?>"><?php echo $size; ?></a>
+                        </li>
+                    <?php } ?>
+                </ul>
             </div>
         <?php } ?>
     </div>
     <div class="ProductCard-body">
         <div>
-            <?php if (rand(0,1)) { ?>
-            <div class="ProductCard-secondaryBadges">
-                <div class="Badge Badge--rectangle" style="--color: #5ce62e">
-                    štítek test
-                </div>
-            </div>
-            <?php } ?>
             <h2 class="ProductCard-title">
                 <?php if (rand(0,1)) { ?>
                     MacBook Pro 15" 2,5 GHz s Retina displejem, 512 GB (2015)
@@ -34,21 +34,14 @@
                 <?php } ?>
             </h2>
         </div>
-        <div class="ProductCard-stock">
-            <?php if (rand(0,1)) { ?>
-            <div class="u-fontMedium u-textColorGreen">Skladem &gt; 5 ks</div>
-            <?php } else { ?>
-                <div class="u-fontMedium u-textColorOrange">Naskladníme do 24 hodin</div>
-            <?php } ?>
-        </div>
     </div>
     <div class="ProductCard-footer">
         <div class="ProductCard-footerContent">
             <div class="ProductCard-priceWrapper">
-                <div class="ProductCard-price"> 76&nbsp;990&nbsp;Kč</div>
-            </div>
-            <div class="ProductCard-quantity">
-                <a href="#basket" class="Btn Btn--secondary Btn--style1 Btn-0--block Btn-xs--block Btn-sm--block ProductCard-btn ">Do košíku</a>
+                <div class="ProductCard-price">76&nbsp;990&nbsp;Kč</div>
+                <?php if (rand(0,1)) { ?>
+                <del class="ProductCard-priceOld">85&nbsp;544&nbsp;Kč</del>
+                <?php } ?>
             </div>
         </div>
     </div>
